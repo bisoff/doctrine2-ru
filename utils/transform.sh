2d2f4127a2b44b5e190fa1e2aefa84acdd7d439c
bis_sk <bisov_r@mail.ru>
2016-01-08 01:17:28 +0300

    sk08.01.16

---
+++ utils/transform.sh
@@ -0,0 +1,28 @@
+= (\d\.\d{1,2}\.)\s
+=== 
+
+= (\d\.\d{1,2}\.\d\.)\s
+==== 
+
+\*\*PHP\*\*\s?\n
+*PHP*\n[source, php]\n----\n
+
+\*\*XML\*\*\n
+----\n*XML*\n[source, XML]\n----\n
+
+<\?php
+[source, php]\n----\n<?php\n
+
+----
+
+
+[source, sql]
+----
+
+----
+
+[source, php]
+----
+
+[source, XML]
+----
