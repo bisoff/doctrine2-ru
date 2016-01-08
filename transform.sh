b9606ed2407ab38c4c8c07f397e049616cac0404
bis_sk <bisov_r@mail.ru>
2016-01-08 14:30:57 +0300

    sk08.01.16

--- transform.sh
+++ transform.sh
@@ -7,10 +7,18 @@
 
 ls "$1"
 [[ $? == 1 ]] && echo Not found && exit
-sed -E -i "s/__/_/g" "$1" # в asciidoc не нужно удвоение
-sed -E -i "s/**/*/g" "$1" # ~
-sed -E -i 's/^= ([[:digit:]])\s/== /g' "$1"
-sed -E -i 's/^= ([[:digit:]]\.[[:digit:]]{1,2}\.)\s/=== /g' "$1"
-sed -E -i 's/^== ([[:digit:]]\.[[:digit:]]{1,2}.[[:digit:]]{1,2}\.)\s/==== /g' "$1"
-sed -E -i 's/^<\?php/[source, php]\n----\n<?php\n/g' "$1"
-sed -E -i 's/^CREATE TABLE /[source, sql]\n----\nCREATE TABLE /g' "$1"
+fname=$(ls "$1")
+echo $fname
+sed -E "s/__/_/g" $fname > "~tmp1" # в asciidoc не нужно удвоение
+sed -E "s/\*\*/*/g" "~tmp1" > "~tmp2" # ~
+sed -E 's/^= /== /g' "~tmp2" > "~tmp3"
+#sed -E 's/^= [[:space:]]*[^[:digit:]]/== /g' "~tmp2" > "~tmp3"
+#sed -E 's/^= ([[:digit:]])[[:space:]]*/== /g' "~tmp2" > "~tmp3"
+sed -E 's/^== [[:digit:]]{1,2}\.[[:digit:]]{1,2}\.[[:space:]]+/=== /g' "~tmp3" > "~tmp4"
+#sed -E 's/^= ([[:digit:]]\.[[:digit:]]{1,2}\.)[[:space:]]*/=== /g' "~tmp3" > "~tmp4"
+sed -E 's/^== ([[:digit:]]{1,2}\.[[:digit:]]{1,2}.[[:digit:]]{1,2}\.)[[:space:]]+/==== /g' "~tmp4" > "~tmp5"
+sed -E 's/^<\?php/[source, php]\n----\n<?php\n/g' "~tmp5" > "~tmp6"
+sed -E 's/^CREATE TABLE /[source, sql]\n----\nCREATE TABLE /g' "~tmp6" > "~tmp7"
+mv -f "$fname" "$fname.bak"
+mv -f "~tmp7" "$fname"
+rm "~tmp1" "~tmp2" "~tmp3" "~tmp4" "~tmp5" "~tmp6" "~tmp7"
