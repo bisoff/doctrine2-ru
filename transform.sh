ce15cf3ccd160a348778a47d69e345b0349f7961
bis_sk <bisov_r@mail.ru>
2016-01-08 12:43:49 +0300

    sk08.01.16

--- transform.sh
+++ transform.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-#transform 08.Работа_с_объектами.asc
-echo "$1"
-sed -E -i 's/__/_/g' -i $1
-sed -E -i 's/^= ([[:digit:]])\s/== /g' $1
-sed -E -i 's/^= ([[:digit:]]\.[[:digit:]]{1,2}\.)\s/=== /g' $1
-sed -E -i 's/^== ([[:digit:]]\.[[:digit:]]{1,2}.[[:digit:]]{1,2}\.)\s/==== /g' $1
-sed -E -i 's/^<\?php/[source, php]\n----\n<?php\n/g' $1
+
+#msys 1.9.x
+#./transform.sh 08.Работа_с_объектами.asc
+#./transform.sh 09*
+#single cmd ex.: sed -E -i 's/^CREATE TABLE /[source, sql]\n----\nCREATE TABLE /g' 09*
+
+ls "$1"
+[[ $? == 1 ]] && echo Not found && exit
+sed -E -i "s/__/_/g" "$1" # в asciidoc не нужно удвоение
+sed -E -i "s/**/*/g" "$1" # ~
+sed -E -i 's/^= ([[:digit:]])\s/== /g' "$1"
+sed -E -i 's/^= ([[:digit:]]\.[[:digit:]]{1,2}\.)\s/=== /g' "$1"
+sed -E -i 's/^== ([[:digit:]]\.[[:digit:]]{1,2}.[[:digit:]]{1,2}\.)\s/==== /g' "$1"
+sed -E -i 's/^<\?php/[source, php]\n----\n<?php\n/g' "$1"
+sed -E -i 's/^CREATE TABLE /[source, sql]\n----\nCREATE TABLE /g' "$1"
